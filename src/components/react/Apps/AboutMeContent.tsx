3001c2ba2b508a809d0fd161e50fbfc4bd1bd231
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-10-28 12:17:38 +0100

    feat(aboutme): add mail + github buttons to about me section
    See title.

--- src/components/react/Apps/AboutMeContent.tsx
+++ src/components/react/Apps/AboutMeContent.tsx
@@ -1,6 +1,6 @@
 export default function AboutMeContent() {
 	return (
-		<div className="about-me-content">
+		<div className="about-me-content flex flex-col gap-4">
 			<h1 className="text-4xl font-cmd text-center">Hello, World!</h1>
 			<p className="font-windows">
 				Hi, I’m Mikkel—front‑end tinkerer, bug whisperer and habitual over-commenter. I
@@ -12,12 +12,39 @@ export default function AboutMeContent() {
 				Interests: clean UI, clever micro‑interactions, monospaced fonts with ligatures, and
 				the occasional cybersecurity rabbit hole. Also: naming things badly so I can rename
 				them later.
-				<br />
-				<br />I ship to Netlify, I read docs for fun, and I only break production once—per
-				project. If you need someone who can design, build, and debug with equal enthusiasm,
-				I’m your Mikkel. Bonus: my dog Bilbo provides moral support and unsolicited code
-				reviews.
 			</p>
+			<section
+				className="flex flex-col justify-center gap-8 bg-windows-white border-2 p-2.5"
+				style={{
+					borderStyle: 'inset',
+				}}
+			>
+				<h2 className="text-2xl font-cmd text-center">Contact Me</h2>
+				<div className="contact-wrapper flex flex-row justify-around">
+					<a
+						href="https://github.com/ExManubis"
+						className="flex flex-col font-windows text-center"
+					>
+						<img
+							src="./icons/github.png"
+							alt="Github Logo"
+							className="w-15 h-15 place-self-center"
+						/>
+						My Github
+					</a>
+					<a
+						href="mailto:mikkel@fejl40.dev"
+						className="flex flex-col font-windows text-center"
+					>
+						<img
+							src="./icons/mail.png"
+							alt="Email icon"
+							className="w-15 h-15 place-self-center"
+						/>
+						E-mail
+					</a>
+				</div>
+			</section>
 		</div>
 	);
 }
